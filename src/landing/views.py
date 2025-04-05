7e06e3330b2491db4f624b103dacb25ea5e7bfbc
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2025-04-06 01:05:04 +0200

    Revert "Enable caching behavior for testing purposes"
    This reverts commit 4442897cc1407b1d4c02a57e4d1cc58186239690.

--- src/landing/views.py
+++ src/landing/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.views.decorators.cache import cache_page
 
-@cache_page(60)
 def landing_view(request):
     return render(request, 'landing/landing.html')
