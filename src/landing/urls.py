d770b753966863b62af570c786b3eb6f811b5ee6
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2025-04-06 14:46:31 +0200

    Route cached page

--- src/landing/urls.py
+++ src/landing/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import landing_view
+from .views import landing_view, cached_page_view
 
 urlpatterns = [
     path('', landing_view, name='landing'),
+    path('cached/', cached_page_view, name='cached')
 ]
