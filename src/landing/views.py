6c1d21acfa271dd68cbe7f2ce0b7181736593bf2
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2025-04-06 14:46:44 +0200

    Create view for cached page

--- src/landing/views.py
+++ src/landing/views.py
@@ -1,4 +1,14 @@
+import datetime
+
+from django.views.decorators.cache import cache_page
 from django.shortcuts import render
 
 def landing_view(request):
     return render(request, 'landing/landing.html')
+
+
+@cache_page(60 * 1)
+def cached_page_view(request):
+    now = datetime.datetime.now()
+    formatted_time = now.strftime("%H:%M:%S")
+    return render(request, 'landing/cached.html', {'time': formatted_time})
